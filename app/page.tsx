f158e86f3018eb7cc041ed93d0329add8719982c
user.email <mrdilmurat777@gmail.com>
2025-05-23 12:12:39 +0600

    feat: add hero section with welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import Hero from "./components/hero/Hero"
 import Layout from "./components/layout/Layout"
 
 export default function Home({ children }: { children: React.ReactNode }) {
   return (
-   <h1 className="text-2xl font-bold">Добро пожаловать в магазин</h1>
+   <Hero />
   )
 }
